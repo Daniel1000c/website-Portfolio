a7d9726829d26ccee4b2d7d00198161ea29d10d7
Daniel Velez <velezdaniel516@gmail.com>
2024-09-12 16:28:24 -0700

    added javascript scroll feature and imgs to project slider

--- studentPortfolio/scripts/script.js
+++ studentPortfolio/scripts/script.js
@@ -1,5 +1,16 @@
 // Create scroll event listener using IntersectionObserver API
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
     //Create constant called observer
-    const observer = new IntersectionObserver
+    const observer = new IntersectionObserver((entries,observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('revealObject'); //Add revealObject class to target element
+                observer.unobserve(entry.target); //Stop observing once element is visible to user
+            }
+        });
+    }, {threshold: 0.1}); // Trigger when about 10% of element is visible to user
+
+    //Create constant called hiddenElement
+    const hiddenElement = document.querySelectorAll('.hideObject');
+    hiddenElement.forEach(el => observer.observe(el));
 });
