66bab091433ced8e6ec139b4ea2f19528462fa40
Daniel Velez <velezdaniel516@gmail.com>
2024-09-12 15:14:51 -0700

    added aria labels to project.html

---
+++ studentPortfolio/scripts/script.js
@@ -0,0 +1,5 @@
+// Create scroll event listener using IntersectionObserver API
+document.addEventListener("DOMContentLoaded", function(){
+    //Create constant called observer
+    const observer = new IntersectionObserver
+});
