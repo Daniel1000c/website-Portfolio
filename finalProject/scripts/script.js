647ad5e275329d5154ac3e98fb003bc870a854b8
Daniel Velez <velezdaniel516@gmail.com>
2024-09-27 13:53:01 -0700

    added villa section

---
+++ finalProject/scripts/script.js
@@ -0,0 +1,19 @@
+//Select all links that need to be jumped
+const jumpLink = document.querySelectorAll('.pageLinkJump');
+
+//Add an click event listener to each link
+jumpLink.forEach(link=> {
+    link.addEventListener('click',function(event){
+        //Prevent default link behavior
+        event.preventDefault();
+
+        //get the id from the link
+        const linkTargetId = this.getAttribute('href');
+        const sectionTarget = document.querySelector(linkTargetId);
+
+        //Smooth scroll section target
+        sectionTarget.scrollIntoView({
+            behavior: 'smooth' //Give link jump a smooth scroll
+        });
+    });
+});
