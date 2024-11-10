4ed5ca1ed6b4d16bacf3947ad29704f95ae337df
Daniel Velez <velezdaniel516@gmail.com>
2024-11-10 01:07:25 -0500

    added site logo, and header imgs for about and contact.pug

--- studentPortfolio/myExpressApp/routes/index.js
+++ studentPortfolio/myExpressApp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Broward Pawn' });
+  res.render('index', { title: '💎 Broward Pawn' });
 });
 
 /*Get about us page */
