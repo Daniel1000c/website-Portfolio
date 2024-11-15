aca5aa3295013d54174e0073e4d3c542d3466b27
Daniel Velez <velezdaniel516@gmail.com>
2024-11-14 19:04:48 -0500

    reworked index page and added about and contact containers

--- studentPortfolio/myExpressApp/routes/index.js
+++ studentPortfolio/myExpressApp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '💎 Broward Pawn' });
+  res.render('index', { title: 'Broward Pawn' });
 });
 
 /*Get about us page */
