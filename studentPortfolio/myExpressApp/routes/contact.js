42b03982b86561092ba3d373f69d9b7a33ec7999
Daniel Velez <velezdaniel516@gmail.com>
2024-11-15 18:57:09 -0800

    added contact form

---
+++ studentPortfolio/myExpressApp/routes/contact.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET Contact page. */
+/*Get contact us page*/
+router.get('/contact', function(req, res, next) {
+  res.render('contact', {title: 'Contact Us'});
+}); 
+
+module.exports = router;
